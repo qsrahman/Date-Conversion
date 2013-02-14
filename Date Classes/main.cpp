2c3fc19c7087e104b7ad38c44cd4ecd33430035a
Sami ur Rahman Qazi <qsrahman@CyberDog.local>
2013-02-14 15:17:06 +0500

    Initial Commit

---
+++ Date Classes/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Date Classes
+//
+//  Created by Sami ur Rahman Qazi on 14/02/2013.
+//  Copyright (c) 2013 Sami ur Rahman Qazi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
